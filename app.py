8557c68a7185bee49e3f251617ca68374c833cdc
marcoychuta-ops <marco.ychuta@gmail.com>
2026-06-07 10:10:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as go
 # --------------------------------------------------
 
 st.set_page_config(
-    page_title="Deteccion de coronavirus",
+    page_title="DETECCION DE CORONAVIRUS",
     page_icon="📚",
     layout="wide"
 )
